94d4df4fe94c8432465fbab3c077dc78e90eaa04
Ahmad Zaghloul <a.mohamed.zaghloul@gmail.com>
2024-09-24 17:03:55 +0200

    Update MyLibrary.swift

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,24 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import UIKit
+
+public struct Test {
+    public static func getDocumentsDirectory() -> URL? {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+    }
+    
+    public static func loadScreenshot() -> UIImage? {
+        guard let directory = getDocumentsDirectory() else { return nil }
+        let filePath = directory.appendingPathComponent("screenshot.png")
+        
+        if FileManager.default.fileExists(atPath: filePath.path) {
+            return UIImage(contentsOfFile: filePath.path)
+        }
+        
+        print("Screenshot not found")
+        return nil
+    }
+
+}
